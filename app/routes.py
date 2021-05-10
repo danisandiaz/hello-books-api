8fd1fed700003e9819a91c2421ba629551c0dd6c
Daniela Sanchez <dani.sanchez077@gmail.com>
2021-05-09 21:27:00 -0700

    add Procfile file, tests, and routes

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,56 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import request, Blueprint, make_response, jsonify
 
-hello_world_bp = Blueprint("hello_world_bp", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def get_hello_world():
-    my_response = "Hello, World!"
-    return my_response
-    
-@hello_world_bp.route("/hello-world/JSON", methods = ["GET"])
-def hello_world_json():
-    return{
-        "name": "Dani!",
-        "message": "Heya!",
-        "hobbies": ["Coding","Painting", "Bad singing"],
-    }, 201
+@books_bp.route("", methods=["GET", "POST"])
+def handle_books():
+    if request.method == "GET":
+        title_query = request.args.get("title")
+        if title_query:
+            books = Book.query.filter_by(title = title_query)
+        else:
+            books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            })
+        return jsonify(books_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"], strict_slashes=False)
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+    if book:
+        if request.method == "GET":
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        elif request.method == "PUT":
+            form_data = request.get_json()
+            book.title = form_data["title"]
+            book.description = form_data["description"]
+            db.session.commit()
+            return make_response(f"Book #{book.id} successfully updated")
+
+        elif request.method == "DELETE":
+            db.session.delete(book)
+            db.session.commit()
+            return make_response(f"Book #{book.id} successfully deleted", 200)
+    else:
+        return make_response("Book not found", 404)
+
